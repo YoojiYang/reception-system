0a860d547bdb8d531b8a2d29e03b0e50b028ccb2
Yang Yoji <yangyooji1946@gmail.com>
2023-10-12 14:40:19 +0900

    prisma index

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,21 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+// let prisma: PrismaClient;
+// declare global {
+//   namespace NodeJS {
+//     interface Global {
+//       prisma: PrismaClient;
+//     }
+//   }
+// }
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!globalThis.prisma) {
+//     globalThis.prisma = new PrismaClient();
+//   }
+//   prisma = globalThis.prisma;
+// }
+
+
 declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
+	var prisma: PrismaClient;
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!globalThis.prisma) {
-    globalThis.prisma = new PrismaClient();
-  }
-  prisma = globalThis.prisma;
-}
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
